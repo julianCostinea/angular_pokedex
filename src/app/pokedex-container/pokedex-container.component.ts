d5365708270f1f0852467704bddc1a5eb47221ee
Emil Julian Costinea <ejc@arp-hansen.dk>
2024-01-20 19:45:33 +0100

    added loading spinner and no matching search message

--- src/app/pokedex-container/pokedex-container.component.ts
+++ src/app/pokedex-container/pokedex-container.component.ts
@@ -3,27 +3,38 @@ import { PokemonListComponent } from '../pokemon-list/pokemon-list.component';
 import { PokemonListService } from '../pokemon-list/pokemon-list.service';
 import { Pokemon } from '../types/pokemon';
 import { PokemonPaginationComponent } from '../pokemon-pagination/pokemon-pagination.component';
+import { NgIf } from '@angular/common';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @Component({
   selector: 'app-pokedex-container',
   standalone: true,
   templateUrl: './pokedex-container.component.html',
   styleUrl: './pokedex-container.component.css',
-  imports: [PokemonListComponent, PokemonPaginationComponent],
+  imports: [
+    PokemonListComponent,
+    PokemonPaginationComponent,
+    NgIf,
+    NgxSpinnerModule,
+  ],
 })
 export class PokedexContainerComponent {
   constructor(private pokemonListService: PokemonListService) {}
 
   pokemons: Pokemon[] = [];
   unfilteredPokemon: Pokemon[] = [];
+  loading = false;
 
   getPokemon(page: number = 0): void {
+    this.loading = true;
     this.pokemonListService.getPokemon(page).subscribe((pokemonList) => {
       this.pokemons = pokemonList.results;
       this.unfilteredPokemon = pokemonList.results;
+      this.loading = false;
     });
   }
 
+  //moved here instead of pokemon-list so that search bar doesnt show removed pokemon
   onRemovePokemon(pokemon: string): void {
     this.pokemons = this.pokemons?.filter((poke) => poke.name !== pokemon);
   }
