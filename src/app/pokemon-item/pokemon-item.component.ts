0e94ee09bfcdfe17d08b156b003ee05c05745c58
Emil Julian Costinea <ejc@arp-hansen.dk>
2024-01-20 18:55:05 +0100

    more styling

--- src/app/pokemon-item/pokemon-item.component.ts
+++ src/app/pokemon-item/pokemon-item.component.ts
@@ -1,15 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Pokemon } from '../types/pokemon';
-import { NgIf } from '@angular/common';
+import { NgIf, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-item',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, NgOptimizedImage],
   templateUrl: './pokemon-item.component.html',
   styleUrl: './pokemon-item.component.css',
 })
 export class PokemonItemComponent {
   @Input() pokemon?: Pokemon;
   @Output() removePokemon = new EventEmitter();
+
+  pokemonImageUrl =
+    'https://images.unsplash.com/photo-1542779283-429940ce8336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80';
 }
